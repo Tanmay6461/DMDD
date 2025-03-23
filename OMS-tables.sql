e9324e7d0aafe94ef6c1c1df202234bbdef732da
Ankitdeopurkar007 <ankitdeopurkar111@gmail.com>
2025-03-23 19:40:23 -0400

    Table for described diagnostics created

--- OMS-tables.sql
+++ OMS-tables.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS Doctor_Details (
     specialization VARCHAR2(50)
 );
 
+CREATE TABLE IF NOT EXISTS Prescribed_Diagnostics (
+    diagnostic_test_id VARCHAR2(50),
+    date_administered DATE NOT NULL,
+    test_result VARCHAR2(100),
+    nurse_id VARCHAR2(50),
+    patient_id VARCHAR2(50),
+    PRIMARY KEY (diagnostic_test_id, patient_id),
+    CONSTRAINT fk_prescribed_diagnostics_patient FOREIGN KEY (patient_id) REFERENCES Patient_Records (patient_id),
+    CONSTRAINT fk_prescribed_diagnostics_test FOREIGN KEY (diagnostic_test_id) REFERENCES Diagnostic_Test (diagnostic_id)
+);

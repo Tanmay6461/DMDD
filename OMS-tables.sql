2fb4710efa56cab63aaa370410ceb3976f85f5ec
sahilk710 <kasliwal.s@northeastern.edu>
2025-03-23 20:09:15 -0400

    Created Medication Information

--- OMS-tables.sql
+++ OMS-tables.sql
@@ -7,3 +7,13 @@ CREATE TABLE Patient_Records (
     last_name VARCHAR2(50) NOT NULL,
     doctor_id VARCHAR2(50) NOT NULL,
     CONSTRAINT fk_patient_doctor FOREIGN KEY (doctor_id) REFERENCES Doctor_Details (doctor_id));
+    
+    
+CREATE TABLE Medication_Information (
+    prescription_id VARCHAR2(50) PRIMARY KEY,
+    medicine_name VARCHAR2(100),
+    medicine_composition VARCHAR2(150),
+    date_administered DATE NOT NULL,
+    patient_id VARCHAR2(50) NOT NULL,
+    nurse_id VARCHAR2(50),
+    CONSTRAINT fk_medication_patient FOREIGN KEY (patient_id) REFERENCES Patient_Records (patient_id));
